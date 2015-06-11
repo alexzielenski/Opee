17f9eafd2960a330e06487fdd8a4a8cff9f81d9e
Alexander Zielenski <alex@alexzielenski.com>
2015-06-10 21:34:49 -0700

    use substitute instead

--- Opee Tests/Opee_Tests.m
+++ Opee Tests/Opee_Tests.m
@@ -10,57 +10,9 @@
 #import <Opee/Opee.h>
 #import <XCTest/XCTest.h>
 
-@interface OPOriginalClass : NSObject
-+ (NSString *)classMethod;
-+ (NSString *)description;
-- (NSString *)instanceMethod;
-- (NSString *)description;
-@end
-
-@implementation OPOriginalClass
-
-+ (BOOL)isSubclassOfClass:(Class)aClass { return YES; }
-+ (NSString *)classMethod { return @"original"; }
-+ (NSString *)description { return @"original"; }
-- (NSString *)instanceMethod { return @"original"; }
-- (NSString *)description { return @"original"; }
-
-@end
-
 OPHook0(NSString *, NSUserName) {
-    return [$O_NSUserName() stringByAppendingString: @"_replaced"];
+    return @"No";
 }
-
-@interface OPSwizzleClass : OPOriginalClass @end
-@implementation OPSwizzleClass
-
-+ (BOOL)isSubclassOfClass:(Class)aClass {
-    return (BOOL)ORIG();
-}
-
-- (NSString *)className {
-    return [ORIG() stringByAppendingString:@"_replaced"];
-}
-
-+ (NSString *)classMethod {
-    return @"replaced";
-}
-
-+ (NSString *)description {
-    return SUPER();
-}
-
-- (NSString *)instanceMethod {
-    return @"replaced";
-}
-
-- (NSString *)description {
-    return SUPER();
-}
-
-@end
-
-
 
 @interface Opee_Tests : XCTestCase @end
 
@@ -69,24 +21,15 @@ OPHook0(NSString *, NSUserName) {
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    [OPSwizzler swizzleClass:[OPSwizzleClass class] forClass:[OPOriginalClass class]];
     OPHookFunction(NSUserName);
+}
+
+- (void)testHooking {
+    XCTAssertEqualObjects(NSUserName(), @"No");
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
-
-- (void)testSwizzle {
-    OPOriginalClass *instance = [[OPOriginalClass alloc] init];
-    XCTAssertEqualObjects([OPOriginalClass classMethod], @"replaced", @"replacing class methods");
-    XCTAssertEqualObjects([instance instanceMethod], @"replaced", @"replacing instance methods");
-    XCTAssertNotEqualObjects([OPOriginalClass description], @"original", @"calling super on class");
-    XCTAssertNotEqualObjects([instance description], @"original", @"calling super on instance");
-    XCTAssertEqual([OPOriginalClass isSubclassOfClass:[NSString class]], YES, @"calling super imp on class");
-    XCTAssertEqualObjects([instance className], @"OPOriginalClass_replaced", @"calling original imp on instance");
-    XCTAssertEqualObjects(NSUserName(), [NSHomeDirectory().lastPathComponent stringByAppendingString:@"_replaced"], @"function hooking");
-}
-
 @end
--- Opee/OPHooker.c
+++ Opee/OPHooker.c
@@ -7,9 +7,20 @@
 //
 
 #include "OPHooker.h"
-void MSHookFunction(void *symbol, void *replace, void **result);
-void *MSFindSymbol(const void *image, const char *name);
-void *MSGetImageByName(const char *file);
+
+typedef void *MSImageRef;
+
+MSImageRef MSGetImageByName(const char *file)
+__asm__("SubGetImageByName");
+void *MSFindSymbol(MSImageRef image, const char *name)
+__asm__("SubFindSymbol");
+
+void MSHookFunction(void *symbol, void *replace, void **result)
+__asm__("SubHookFunction");
+
+//void MSHookFunction(void *symbol, void *replace, void **result);
+//void *MSFindSymbol(const void *image, const char *name);
+//void *MSGetImageByName(const char *file);
 
 int OPHookFunctionPtr(void *symbol, void *replace, void **result) {
     MSHookFunction(symbol, replace, result);
@@ -21,5 +32,5 @@ void *OPGetImageByName(const char *file) {
 }
 
 void *OPFindSymbol(const void *image, const char *name) {
-    return MSFindSymbol(image, name);
+    return MSFindSymbol((MSImageRef)image, name);
 }
